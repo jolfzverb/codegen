254531275174bb93e177379022c4494a6ebde293
Yury Usishchev <jolfzverb@gmail.com>
2026-04-15 01:04:00 +0200

    remove more slop

--- internal/generator/astbuilder/simple_type_builder.go
+++ internal/generator/astbuilder/simple_type_builder.go
@@ -68,53 +68,6 @@ func (stb *SimpleTypeBuilder) Build() ast.Expr {
 	return expr
 }
 
-// BuildAsIdent creates an ast.Ident if there's only one element, panics otherwise
-func (stb *SimpleTypeBuilder) BuildAsIdent() *ast.Ident {
-	if len(stb.elements) != 1 {
-		panic("BuildAsIdent requires exactly one element")
-	}
-	return ast.NewIdent(stb.elements[0])
-}
-
-// BuildAsSelector creates an ast.SelectorExpr if there are multiple elements, panics otherwise
-func (stb *SimpleTypeBuilder) BuildAsSelector() *ast.SelectorExpr {
-	if len(stb.elements) < 2 {
-		panic("BuildAsSelector requires at least two elements")
-	}
-
-	var expr ast.Expr = ast.NewIdent(stb.elements[0])
-	for i := 1; i < len(stb.elements); i++ {
-		expr = &ast.SelectorExpr{
-			X:   expr,
-			Sel: ast.NewIdent(stb.elements[i]),
-		}
-	}
-	return expr.(*ast.SelectorExpr)
-}
-
-// ElementCount returns the number of elements in the type
-func (stb *SimpleTypeBuilder) ElementCount() int {
-	return len(stb.elements)
-}
-
-// HasElements returns true if the type has any elements
-func (stb *SimpleTypeBuilder) HasElements() bool {
-	return len(stb.elements) > 0
-}
-
-// GetElements returns a copy of the elements slice
-func (stb *SimpleTypeBuilder) GetElements() []string {
-	elements := make([]string, len(stb.elements))
-	copy(elements, stb.elements)
-	return elements
-}
-
-// Clear removes all elements from the type
-func (stb *SimpleTypeBuilder) Clear() *SimpleTypeBuilder {
-	stb.elements = make([]string, 0)
-	return stb
-}
-
 // Helper methods for common types
 
 // String creates a simple type builder for "string"
